c48a107a87eda1fdc8a2c3b504ff933304cf4d95
Andrey Fedorov <andrei.fedorov@inbox.ru>
2023-08-17 10:53:32 +0300

    Add new class

--- src/test/java/CompanyBusinessContract.java
+++ src/test/java/CompanyBusinessContract.java
@@ -1,8 +1,10 @@
+import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static io.restassured.RestAssured.authentication;
+
 import static io.restassured.RestAssured.given;
 
 
@@ -11,6 +13,7 @@ public class TestApi {
 
 
     @Test
+    @
     public void get_UserToken() {
       String uri = "https://x-clients-be.onrender.com/";
         Map<String, String> request = new HashMap<>();
@@ -21,39 +24,52 @@ public class TestApi {
                 .when()
                 .post(uri + "auth/login")
                 .then().log().body()
-               .extract()
-               .response().jsonPath().get("userToken");
+               .extract().path("userToken");
+        System.out.println(userToken);
+
+
 
 
     }
     @Test
     public void create_Company(){
         String uri = "https://x-clients-be.onrender.com/";
+        Map<String, String> request = new HashMap<>();
+        request.put("username", "leonardo");
+        request.put("password", "leads");
+        String userToken = given().contentType("application/json")
+                .body(request)
+                .when()
+                .post(uri + "auth/login")
+                .then().log().body()
+                .extract().path("userToken");
+
         String companyName = "Кофемания";
         String companyDescription = "Кофейня";
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTIxODYyMTAsImV4cCI6MTY5MjE4NzExMH0.cbJ_kNHfLMQJBOzBwT5PKyun7mOAWCQfD6vA6T0M2Lg";
-
-        given()
-                .header("x-client-token", token)
+       int id =  given()
+                .header("x-client-token", userToken)
                 .contentType("application/json; charset=utf-8")
                 .body("{\"name\": \"" + companyName + "\",\"description\": \"" + companyDescription + "\"}")
                 .when()
                 .post(uri + "company")
                 .then()
-                .statusCode(201)
                 .contentType("application/json; charset=utf-8")
                 .extract().path("id");
+        System.out.println(id);
 
     }
 
     @Test
-    public void delete_Company(){
+    public void get_Companies(){
+        String uri = "https://x-clients-be.onrender.com/";
+        List name = given()
+                    .when().get(uri+"company")
+                    .then()
+                    .extract().path("name");
+        System.out.print(name);
+        }
 
     }
 
-
-
-
-}
 
 
