9663b8d9b0bf73ce161a35259d3da11fa2082525
Andrey Fedorov <andrei.fedorov@inbox.ru>
2023-08-21 13:30:43 +0300

    commit tests

--- src/test/java/Employee/EmployeeContractTest.java
+++ src/test/java/Employee/EmployeeContractTest.java
@@ -83,7 +83,7 @@ public class EmployeeContractTest {
     }
 
     @Test
-    @Description("Вывод имен сотрудников, которые работают в кофейне")
+    @Description("Вывод информации о сотрудниках, которые работают в кофейне")
     public void getEmployee() {
         File propFile = new File("src/test/java/employee.properties");
         Properties properties = new Properties();
@@ -93,10 +93,10 @@ public class EmployeeContractTest {
             throw new RuntimeException(e);
         }
         String uri = properties.getProperty("URI");
-        List name = given()
+        String name = given()
                 .when().get(uri + "employee?company=715")
                 .then()
-                .extract().path("firstName");
+                .extract().response().asPrettyString();
         System.out.print(name);
 
 
@@ -145,5 +145,86 @@ public class EmployeeContractTest {
                 .extract().response().asPrettyString();
     }
 
+    @Test
+    @Description("Вывод информации о сотруднике по Id")
+    public void getEmployeeById() {
+        File propFile = new File("src/test/java/employee.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(propFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String uri = properties.getProperty("URI");
+        String name = given()
+                .when().get(uri + "employee/"+1630)
+                .then()
+                .extract().response().asPrettyString();
+        System.out.print(name);
+    }
+
+    @Test
+    @Description("Изменение авторизованного сотрудника")
+    public void changeEmployeeByID(){
+        File propFile = new File("src/test/java/employee.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(propFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String uri = properties.getProperty("URI");
+        String username = properties.getProperty("USER");
+        String password = properties.getProperty("PASS");
+        Map<String, String> request = new HashMap<>();
+        request.put("username", username);
+        request.put("password", password);
+        String userToken = given().contentType("application/json")
+                .body(request)
+                .when()
+                .post(uri + "auth/login")
+                .then().log().body()
+                .extract().path("userToken");
+
+        Employee employee = new Employee(1630,"Dmitriy","Kolesov","",715,"hello@cofemaniya.ru",null,"8172858585","2023-08-17T10:07:33.189Z",true);
+        String emp =  given()
+                .header("x-client-token", userToken)
+                .contentType("application/json; charset=utf-8")
+                .body(employee)
+                .when()
+                .patch(uri + "employee/"+1630)
+                .then()
+                .contentType("application/json; charset=utf-8")
+                .extract().response().asPrettyString();
+        System.out.print(emp);
+
+
+    }
+
+    @Test
+    @Description("Создание не авторизованного сотрудника")
+    public void changeNotAutEmployee(){
+        File propFile = new File("src/test/java/employee.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(propFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String uri = properties.getProperty("URI");
+
+
+        Employee employee = new Employee(5,"Dmitriy","Kolesov","",715,"hello@cofemaniya.ru",null,"8172858585","2023-08-17T10:07:33.189Z",true);
+        String message =  given()
+                .contentType("application/json; charset=utf-8")
+                .body(employee)
+                .when()
+                .patch(uri + "employee/"+1630)
+                .then()
+                .statusCode(401)
+                .extract().body().path("message");
+        assertEquals("Unauthorized", message);
+
+    }
 
 }
